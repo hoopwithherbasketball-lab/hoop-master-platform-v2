e153893c0f5d210dd1d1164eb4642726f4f97915
lrevell8-arch <lrevell8@gmail.com>
2026-05-31 01:02:38 -0400

    update-start-dev-bat

--- start-dev.bat
+++ start-dev.bat
@@ -1,6 +1,12 @@
 @echo off
 cd /d "%~dp0apps\web"
 echo Starting Hoop With Her dev server...
+echo Press Ctrl+C to stop.
 echo.
 node ..\..\node_modules\vite\bin\vite.js --host
-pause
+if errorlevel 1 (
+  echo.
+  echo Server crashed. Retrying in 3 seconds...
+  timeout /t 3 /nobreak >nul
+  node ..\..\node_modules\vite\bin\vite.js --host
+)
